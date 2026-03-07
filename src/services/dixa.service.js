8dcac26cc7181244f294991dc8f8cec8a5675afc
Rob van den Boogaard <rob@cxdigital.nl>
2026-03-07 13:55:52 +0100

    Update dixa.service.js

--- src/services/dixa.service.js
+++ src/services/dixa.service.js
@@ -29,7 +29,7 @@ async function getConversations(filters = {}) {
     {
       params: { created_after: createdAfter, created_before: createdBefore },
       headers: { 'Authorization': `bearer ${config.apiKey}`, 'Content-Type': 'application/json' },
-      timeout: 30000,
+      timeout: 8000,
     }
   );
 
